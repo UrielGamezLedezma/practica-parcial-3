3afa8d2df156aa151f822e76ff05c821dfbbf0d4
UrielGamezLedezma <uriel.gamezledezma.gen23@cbtis105.edu.mx>
2025-06-11 09:46:37 -0600

    terminado

--- ClientApp/Presentacion2.tsx
+++ ClientApp/Presentacion2.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 type Equipo = {
      "Id": string,
@@ -13,26 +13,47 @@ type Equipo = {
 }
 
 const Presentacion2 = () => {
+    const [equipo, setEquipo] = useState<Equipo>();
+
     //Manejo de datos 
     const cargarDatos = async () => {
         const resp = await fetch("/mi-proyecto/presentacion");
         if (resp.ok) {
             const datos = await resp.json();
             console.log(datos);
+            setEquipo(datos);
         }
     }
 
     useEffect(() => {
         cargarDatos();
     }, []);
+
+const formatoFecha = (fecha: string | undefined) => {
+    if(fecha){
+
+    
+ const formato = new Intl.DateTimeFormat("es-MX", {year: "numeric", month: "2-digit", day: "2-digit"});
+ const dt = new Date(fecha); 
+
+ return formato.format(dt);
+ }
+ return undefined;
+}
+
     //Vista
     return (
         <>
-            <div className="display-4">Nombre del cbtis</div>
-            <div className="h1">Integrantes</div>
-            <div className="h2">Nombre del integrante 1</div>
-            <div className="h2">Nombre del integrante 2</div>
-            <div className="h1">Nombre del proyecto</div>
+            <div className="display-4 text-center">{equipo?.Escuela}</div>
+            <div className="h1 text-center mt-4">{equipo?.Carrera}</div>
+            <div className="h1 text-center mt-4">{equipo?.DatosSemestrales}</div>
+            <div className="h1 text-center">{equipo?.Grupo}</div>
+            <div className="h1 text-center mt-4">Integrantes</div>
+            <div className="h2 text-center text-primary">{equipo?.Integrante1}</div>
+            <div className="h2 text-center text-primary">{equipo?.Integrante2}</div>
+            <div className="h1 text-center mt-4">Nombre del proyecto</div>
+            <div className="h3 text-center">{equipo?.Proyecto}</div>
+            <div className="h4 text-center mt-4">{formatoFecha(equipo?.Fecha)}</div>
         </>
     )
 }
